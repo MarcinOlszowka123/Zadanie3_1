730436c90471173fa62606fb104312224892b393
MarcinOlszowka <marcinolszowka34@gmail.com>
2025-11-23 15:53:33 +0000

    w2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
-public class choinka {
+public class Main {
   public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     System.out.println("Podaj wysokosc choinki: ");
     int height = 0;
     try {
-      height = Integer.parseInt(scan.nextLine().trim());
+      height = Integer.parseInt(sc.nextLine().trim());
     } catch (Exception e) {
       height = 5;
     }
-
     System.out.println("Podaj znak:np(*) ");
-    String znak = scan.nextLine().trim();
+    String znak = sc.nextLine().trim();
     if (znak.length() == 0)
       znak = "*";
     char c = znak.charAt(0);
-  }
+    for (int i = 1; i < height; i++) {
+      int stars = 2 * i + 1;
+      int spaces = height - i - 1;
+      for (int j = 0; j < spaces; j++)
+        System.out.print("");
+      for (int j = 0; j < stars; j++)
+        System.out.print(c);
+      System.out.println("");
+    }
 
+  }
 }
