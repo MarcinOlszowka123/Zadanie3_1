4f5f453f61a363f89c72be22a56212f940ad5e25
MarcinOlszowka <marcinolszowka34@gmail.com>
2025-11-24 08:14:37 +0000

    W6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,14 +15,23 @@ public class Main {
     if (znak.length() == 0)
       znak = "*";
     char c = znak.charAt(0);
-    for (int i = 1; i < height; i++) {
-      int stars = i + 1;
-      int spaces = height - i - 1;
-      for (int j = 0; j < spaces; j++)
-        System.out.print("");
-      for (int j = 0; j < stars; j++)
-        System.out.print(c);
-      System.out.println("");
+    System.out.println("Podaj ile ma mieć warstw:");
+    int warstwy = 0;
+    try {
+      warstwy = Integer.parseInt(sc.nextLine().trim());
+    } catch (Exception e) {
+      warstwy = 1;
+    }
+    for (int s = 0; s <= warstwy; s++) {
+      for (int i = 0; i < height; i++) {
+        int stars = i + 1;
+        int spaces = height - i - 1;
+        for (int j = 0; j < spaces; j++)
+          System.out.print("");
+        for (int j = 0; j < stars; j++)
+          System.out.print(c);
+        System.out.println("");
+      }
     }
   }
 }
